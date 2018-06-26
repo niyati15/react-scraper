9904a3c840fc63d1827e1e2480c87d528b5377c2
Yumna Akhtar <yumnaakhtar@hotmail.com>
2018-06-26 11:15:43 -0700

    add server.js connections

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("expresss");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//define middleware
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+//serve up static assets (usuallt on heroku)
+if(process.env.NODE_ENV === "production")
+{
+    app.use(express.static("client/buid"));
+}
+//add all routes
+app.use(routes);
+//connection to MONGO DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+//starts server
+app.listen(PORT, function() {
+    console.log("Servernow listening on PORT: ", PORT);
+});
