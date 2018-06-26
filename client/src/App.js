51336623ef915c95e542db14fadb3823a6d64cc3
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-06-26 13:49:58 -0700

    package.json edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import SearchCard from "./components/SearchCard"
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <SearchCard>
+      </SearchCard>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,7 +14,7 @@ if(process.env.NODE_ENV === "production")
     app.use(express.static("client/buid"));
 }
 //add all routes
-app.use(routes);
+// app.use(routes);
 //connection to MONGO DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 //starts server
