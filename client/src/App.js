03dcd8a7e6e4784020ff115454aa135e69c5904a
lesleyfon <lesleybanadzem2017@gmail.com>
2018-06-26 14:18:24 -0700

    created the navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SearchCard from "./components/SearchCard/SearchCard.js"
 import Jumbotron from "./components/Jumbotron/Jumbotron.js"
+import Navbar from "./components/Navbar/Navbar.js"
 // import logo from './logo.svg';
 // import './App.css';
 
@@ -8,8 +9,11 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Navbar>
+        </Navbar>
       <Jumbotron>
         </Jumbotron>
+        
       <SearchCard>
       </SearchCard>
       </div>
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = props => (
+    <nav class="navbar fixed-top navbar-light bg-light">
+        <h2>NYT </h2>
+        <a class="navbar-brand" href="#">Home</a>
+        <a class="navbar-brand" href="#">Saved</a>
+    </nav>
+)
+
+export default Navbar;
