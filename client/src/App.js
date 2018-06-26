5ead10151d59731b8f111f5528b04c2e0a0c94f2
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-06-26 14:28:07 -0700

    app.js edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import SearchCard from "./components/SearchCard/SearchCard.js"
 import Jumbotron from "./components/Jumbotron/Jumbotron.js"
+import Container from "./components/Container/Container.js"
 // import logo from './logo.svg';
 // import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className = "container">
       <Jumbotron>
         </Jumbotron>
       <SearchCard>
